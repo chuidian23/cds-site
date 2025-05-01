2655dbf741afbe013eaa51969d388817645a9d3c
chuidian23 <leonardchuidian@gmail.com>
2025-05-02 03:54:45 +0800

    Delete frontend directory

--- frontend/src/components/scheduleUtils.js
+++
@@ -1,6 +0,0 @@
-export const validateSchedule = (schedule) => {
-  if (!Array.isArray(schedule)) return [];
-  return schedule.filter(
-    (session) => session.date || (session.start_date && session.end_date)
-  );
-};
